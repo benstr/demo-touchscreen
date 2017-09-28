2fe0fb7914d49be44a82c02af48b13372235016c
benstr <ben@benstrahan.com>
2017-09-28 18:14:53 -0500

    mo style

--- main.js
+++ main.js
@@ -7,11 +7,12 @@ app.on('window-all-closed', () => {
 
 app.on('ready', () => {
 
-	var ui = new BrowserWindow({
-		height: 450,
-		width: 600,
-		resizable: false
+  var ui = new BrowserWindow({
+		height: 444,
+		width: 800,
+		resizable: true
 	});
+  // ui.maximize();
 	ui.loadURL('file://' + __dirname + '/ui.html');
 
 	ui.on('closed', () => {
